b6673e3d2b55ba95987efd0c50d1510c06d38e61
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-10-07 13:32:44 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,24 +1,44 @@
+import os.path
+
+
 class File_processing:
     """Class that performs statistical processing of a text file (counts characters, words and sentences)"""
 
     def __init__(self, filepath):
-        with open(filepath, 'r') as file:
-            output = file.read()
-            self.__content = output
-            self.__chars = len(output)
-            self.__words = len(output.split())
-            self.__sentences = len(output.split('.'))
+        if not os.path.exists(filepath):
+            raise Exception("there's no such file")
+        self.__filepath = filepath
+        self.__content = "content"
+        self.__chars = 0
+        self.__words = 0
+        self.__sentences = 0
 
     def content(self):
+        with open(self.__filepath, 'r') as file:
+            output = file.readlines()
+            for data in output:
+                self.__content = output
         return self.__content
 
     def chars(self):
+        with open(self.__filepath, 'r') as file:
+            output = file.readlines()
+            for data in output:
+                self.__chars = len(output)
         return self.__chars
 
     def words(self):
+        with open(self.__filepath, 'r') as file:
+            output = file.readlines()
+            for data in output:
+                self.__words = len(output.split())
         return self.__words
 
     def sentences(self):
+        with open(self.__filepath, 'r') as file:
+            output = file.readlines()
+            for data in output:
+                self.__sentences = len(output.split('.'))
         return self.__sentences
 
 
