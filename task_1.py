d4dd48c9c771511bdc92077535db6968564dfb9f
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-10-21 00:11:43 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,53 +1,72 @@
 class Product:
-    """Class that describes a product (contains price, description, dimension, quantity)"""
+    """Class that describes a product (contains name, price, description, dimension)"""
 
-    def __init__(self, price=0, descrip="description", dimen="dimension", quantity=0):
-        dimen_dictionary = {"dimension", "XS", "S", "M", "L", "XL"}
-        if not isinstance(price, (int, float)) or not isinstance(descrip, str) or not isinstance(quantity, int):
+    def __init__(self, price, descrip, dimen):
+        dimen_dictionary = {"XS", "S", "M", "L", "XL"}
+        if not isinstance(price, (int, float)) or not isinstance(descrip, str):
             raise TypeError
-        if price < 0 or quantity < 0:
-            raise ValueError
+        if not descrip or not dimen:
+            raise ValueError("no data")
+        if price < 0:
+            raise ValueError("price cannot be less than zero")
         if dimen not in dimen_dictionary:
-            raise ValueError("acceptable dimension input: dimension, XS, S, M, L, XL")
+            raise ValueError("acceptable dimension input: XS, S, M, L, XL")
         self.price = price
-        self.description = descrip
-        self.dimension = dimen
-        self.quantity = quantity
+        self.__description = descrip
+        self.__dimension = dimen
 
 
 class Customer:
     """Class that describes a customer (contains surname, name, patronymic, phone number)"""
 
-    def __init__(self, surname="surname", name="name", patronym="patronymic", number="phone number"):
-        self.surname = surname
-        self.name = name
-        self.patronymic = patronym
-        self.number = number
+    def __init__(self, surname, name, patronym, phone):
+        if not surname or not name or not patronym or not phone:
+            raise ValueError("no data")
+        self.__surname = surname
+        self.__name = name
+        self.__patronymic = patronym
+        self.__number = phone
 
 
 class Order:
     """Class that contains data about the customer and products, calculates the total order value"""
-    order_number = 0
+    __order_number = 0
 
-    def __init__(self, customer, *products):
+    def __init__(self, customer = None):
         if not isinstance(customer, Customer):
-            raise ValueError
-        self.customer = customer
-        self.__total = 0
-        for product in products:
-            if not isinstance(product, Product):
-                raise ValueError
-            self.product = product
-            self.__total += product.quantity * product.price
-        Order.order_number += 1
+            raise TypeError
+        self.__customer = customer
+        self.__products = []
+        Order.__order_number += 1
+
+    def add(self, product):
+        """Method of class Order that add the product to order"""
+        if not isinstance(product, Product):
+            raise TypeError
+        self.__products.append(product)
+
+    def remove(self, product):
+        """Method of class Order that remove the product from order"""
+        if not isinstance(product, Product):
+            raise TypeError
+        if not self.__products:
+            raise ValueError("no data")
+        self.__products.remove(product)
 
     def get_total(self):
-        return f'order №{Order.order_number}: {self.__total}'
+        """Method of class Order that counts and returns the total price of the order"""
+        total = 0
+        for product in self.__products:
+            total += product.price
+        return f'order №{Order.__order_number}: {"{:,.2f}".format(total)}'
 
 
-product1 = Product(1.33, "description", "XL", 5)
-product2 = Product(2, "description", "XS", 10)
-customer = Customer()
-order = Order(customer, product1, product2)
-print(order.get_total())
-
+product1 = Product(5.99, "description1", "S")
+product2 = Product(12, "description2", "XL")
+customer1 = Customer("Bykova", "Polina", "Olegovna", 3452345768)
+order1 = Order(customer1)
+order1.add(product1)
+order1.add(product2)
+print(order1.get_total())
+order1.remove(product2)
+print(order1.get_total())
