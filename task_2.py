2547c49823f2892b3da090fa467fb5e40004a631
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-10-21 00:12:23 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -8,40 +8,38 @@ class File_processing:
         if not os.path.exists(filepath):
             raise Exception("there's no such file")
         self.__filepath = filepath
-        self.__content = "content"
-        self.__chars = 0
-        self.__words = 0
-        self.__sentences = 0
-
-    def content(self):
-        with open(self.__filepath, 'r') as file:
-            output = file.readlines()
-            for data in output:
-                self.__content = output
-        return self.__content
 
     def chars(self):
+        """Characters-counting method of class File_processing"""
         with open(self.__filepath, 'r') as file:
-            output = file.readlines()
-            for data in output:
-                self.__chars = len(output)
-        return self.__chars
+            content = file.readlines()
+            chars = 0
+            for line in content:
+                chars += len(line)
+        return chars
 
     def words(self):
+        """Words-counting method of class File_processing"""
         with open(self.__filepath, 'r') as file:
-            output = file.readlines()
-            for data in output:
-                self.__words = len(output.split())
-        return self.__words
+            content = file.readlines()
+            words = 0
+            for line in content:
+                words += len(line.split())
+        return words
 
     def sentences(self):
+        """Sentences-counting method of class File_processing"""
         with open(self.__filepath, 'r') as file:
-            output = file.readlines()
-            for data in output:
-                self.__sentences = len(output.split('.'))
-        return self.__sentences
+            content = file.readlines()
+            sentences = 0
+            for line in content:
+                line = line.replace("\n", "")
+                line = line.replace("\r", "")
+                line = line.replace('?', '.')
+                line = line.replace('!', '.')
+                sentences += line.count('.')
+        return sentences
 
 
 file1 = File_processing('D:\\PycharmProjects\\lab_2.2\\text.txt')
-print(file1.content())
 print("Characters: ", file1.chars(), " Words: ", file1.words(), " Sentences: ", file1.sentences())
